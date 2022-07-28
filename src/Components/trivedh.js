055d9824c3c8612808ced2af54013d83405e0e17
Trivedh-GitHub <KARLA.TRIVEDH@CAPGEMINI.COM>
2022-07-28 20:08:45 +0530

    version1

---
+++ src/Components/trivedh.js
@@ -0,0 +1,14 @@
+i/* mport React from "react";
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>404 Page not found</h1>
+      </>
+    );
+  }
+}
+
+export default NotFound;
+ */
